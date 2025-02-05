046d8790a3c7fc287a7902b8578777c606c89113
Martin Colbourne <martinlambc@gmail.com>
2025-02-05 19:06:02 +0100

    update test with assertation that the function returns the secret string

--- test/test_get_api_key.py
+++ test/test_get_api_key.py
@@ -16,36 +16,34 @@ def aws_creds():
     os.environ["AWS_DEAFULT_REGION"] = "eu-west-2"
 
 
+secret_string = "Fake API code"
+
+
 @pytest.fixture(scope="function")
 def secrets_client(aws_creds):
     with mock_aws():
         secrets_client = boto3.client("secretsmanager")
-
-        secret_string = "Fake API code"
-
         secrets_client.create_secret(
             Name="Guardian-API-Key", SecretString=secret_string
         )
-
         yield secrets_client
+
 
 @pytest.fixture(scope="function")
 def invalid_secrets_client(aws_creds):
     with mock_aws():
         secrets_client = boto3.client("secretsmanager")
-
         secret_string = "Fake API code"
-
         secrets_client.create_secret(
             Name="Other-API-Key", SecretString=secret_string
         )
-
         yield invalid_secrets_client
 
 
-def test_returns_string(secrets_client):
+def test_returns_secret_string(secrets_client):
     result = get_api_key()
     assert type(result) == str
+    assert result == secret_string
 
 
 def test_raises_client_error_if_client_error_occurs(invalid_secrets_client):
