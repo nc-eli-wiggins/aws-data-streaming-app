8b07d46d8aa560176466eeac399a08d99cf3a755
Martin Colbourne <martinlambc@gmail.com>
2025-02-05 15:51:47 +0100

    add get_api_key and tests

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-
+import boto3
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
     pass
 
 
 def get_api_key():
-    pass
+    
+    region_name = "eu-west-2"
+    secret_name = "Guardian-API-Key"
+
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        raise e
+
+    return get_secret_value_response["SecretString"]
 
 
 def request_content():
---
+++ test/test_get_api_key.py
@@ -0,0 +1,53 @@
+from main import get_api_key
+
+from moto import mock_aws
+import boto3
+from botocore.exceptions import ClientError
+import pytest
+import os
+
+
+@pytest.fixture(scope="function")
+def aws_creds():
+    os.environ["AWS_ACCESS_KEY_ID"] = "test"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "test"
+    os.environ["AWS_SECURITY_TOKEN"] = "test"
+    os.environ["AWS_SESSION_TOKEN"] = "test"
+    os.environ["AWS_DEAFULT_REGION"] = "eu-west-2"
+
+
+@pytest.fixture(scope="function")
+def secrets_client(aws_creds):
+    with mock_aws():
+        secrets_client = boto3.client("secretsmanager")
+
+        secret_string = "Fake API code"
+
+        secrets_client.create_secret(
+            Name="Guardian-API-Key", SecretString=secret_string
+        )
+
+        yield secrets_client
+
+@pytest.fixture(scope="function")
+def invalid_secrets_client(aws_creds):
+    with mock_aws():
+        secrets_client = boto3.client("secretsmanager")
+
+        secret_string = "Fake API code"
+
+        secrets_client.create_secret(
+            Name="Other-API-Key", SecretString=secret_string
+        )
+
+        yield invalid_secrets_client
+
+
+def test_returns_string(secrets_client):
+    result = get_api_key()
+    assert type(result) == str
+
+
+def test_raises_client_error_if_client_error_occurs(invalid_secrets_client):
+    with pytest.raises(ClientError):
+        get_api_key()
