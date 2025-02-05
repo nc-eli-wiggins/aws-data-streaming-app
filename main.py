40e7179a3b14b21e4064043a9b6604a150cb8d13
Martin Colbourne <martinlambc@gmail.com>
2025-02-05 20:31:45 +0100

    add request content function and tests

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 import boto3
 from botocore.exceptions import ClientError
+from requests import get as get_request
 
 def lambda_handler(event, context):
-    pass
+    if not event.get("queryStringParameters"):
+        pass
+    api_key = get_api_key()
 
 
-def get_api_key():
-    
+def get_api_key() -> str:
     region_name = "eu-west-2"
     secret_name = "Guardian-API-Key"
-
+    
     session = boto3.session.Session()
     client = session.client(service_name="secretsmanager", region_name=region_name)
-
+    
     try:
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
         raise e
-
+    
     return get_secret_value_response["SecretString"]
 
 
-def request_content():
-    pass
+def request_content(api_key: str, search_term: str, from_date: str = None)  -> dict | None:
+    
+    url = f'https://content.guardianapis.com/search?q={search_term}'
 
+    if from_date:
+        url += f'&from-date={from_date}'
+    
+    response = get_request(url + f'&api-key={api_key}')
 
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+    
 def post_to_sqs():
     pass
---
+++ test/test_request_content.py
@@ -0,0 +1,43 @@
+import pytest
+from requests.exceptions import RequestException
+from unittest.mock import patch, Mock
+
+from main import request_content
+
+
+@pytest.fixture
+def mock_200_response():
+    mock_200_response = Mock()
+    mock_200_response.status_code = 200
+    mock_200_response.json.return_value = {}
+    return mock_200_response
+
+
+@pytest.fixture
+def mock_500_response():
+    mock_500_response = Mock()
+    mock_500_response.status_code = 500
+    return mock_500_response
+
+
+@patch("main.get_request")
+def test_uses_parameters_in_request(mock_get_request, mock_200_response):
+    mock_get_request.return_value = mock_200_response
+    request_content("test-API-key", "test-search-term", "2023-03-28")
+    mock_get_request.assert_called_with(
+        "https://content.guardianapis.com/search?q=test-search-term&from-date=2023-03-28&api-key=test-API-key"
+    )
+
+
+@patch("main.get_request")
+def test_returns_dict_for_200_response(mock_get_request, mock_200_response):
+    mock_get_request.return_value = mock_200_response
+    output = request_content("test-API-key", "test-search-term", "2023-03-28")
+    assert isinstance(output, dict)
+
+
+@patch("main.get_request")
+def test_returns_none_for_500_response(mock_get_request, mock_500_response):
+    mock_get_request.return_value = mock_500_response
+    output = request_content("test-API-key", "test-search-term", "2023-03-28")
+    assert output is None
