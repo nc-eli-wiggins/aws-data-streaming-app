f6872133712e9a67055c311ef060c64e6179b24d
Martin Colbourne <martinlambc@gmail.com>
2025-02-06 17:12:24 +0100

    add test data

---
+++ test/test_data/prepared_messages.py
@@ -0,0 +1,85 @@
+from json import dumps
+
+
+prepared_messages = [
+    {
+        "Id": "trump-vengeance-critics",
+        "MessageBody": dumps({
+            "webPublicationDate": "2025-01-27T11:00:42Z",
+            "webTitle": "Here's how Trump's vengeance machine works | Robert Reich",
+            "webUrl": "https://www.theguardian.com/commentisfree/2025/jan/27/trump-vengeance-critics"
+        })
+    },
+    {
+        "Id": "tell-us-do-you-use-a-fax-machine",
+        "MessageBody": dumps({
+            "webPublicationDate": "2024-10-30T16:28:31Z",
+            "webTitle": "Tell us: do you use a fax machine?",
+            "webUrl": "https://www.theguardian.com/technology/2024/oct/30/tell-us-do-you-use-a-fax-machine"
+        })
+    },
+    {
+        "Id": "nobel-prize-physics-john-hopfield-geoffrey-hinton-machine-learning",
+        "MessageBody": dumps({
+            "webPublicationDate": "2024-10-08T12:53:51Z",
+            "webTitle": "Machine learning pioneers win Nobel prize in physics",
+            "webUrl": "https://www.theguardian.com/science/2024/oct/08/nobel-prize-physics-john-hopfield-geoffrey-hinton-machine-learning"
+        })
+    },
+    {
+        "Id": "coffee-machine-types",
+            "MessageBody": dumps({
+            "webPublicationDate": "2024-11-21T18:00:01Z",
+            "webTitle": "How to choose the right type of coffee machine for you",
+            "webUrl": "https://www.theguardian.com/thefilter/2024/nov/21/coffee-machine-types"
+        })
+    },
+    {
+        "Id": "beezel-review-horror-aaron-fradkin",
+        "MessageBody": dumps({
+            "webPublicationDate": "2024-12-31T11:00:43Z",
+            "webTitle": "Beezel review – impish jump-scare machine follows single house's horrific history",
+            "webUrl": "https://www.theguardian.com/film/2024/dec/31/beezel-review-horror-aaron-fradkin"
+        })
+    },
+    {
+        "Id": "graham-potter-human-heart-west-ham-gormless-machine",
+        "MessageBody": dumps({
+            "webPublicationDate": "2025-01-10T08:00:36Z",
+            "webTitle": "Graham Potter's task is to put human heart back in West Ham's gormless machine | Jonathan Liew",
+            "webUrl": "https://www.theguardian.com/football/2025/jan/10/graham-potter-human-heart-west-ham-gormless-machine"
+        })
+    },
+    {
+        "Id": "kate-mckinnon-watching-succession-made-me-return-to-king-lear",
+        "MessageBody": dumps({
+            "webPublicationDate": "2024-12-14T10:00:14Z",
+            "webTitle": "Kate McKinnon: 'Watching Succession made me return to King Lea'",
+            "webUrl": "https://www.theguardian.com/books/2024/dec/14/kate-mckinnon-watching-succession-made-me-return-to-king-lear"
+        })
+    },
+    {
+        "Id": "fani-willis-georgia-trump-trial",
+        "MessageBody": dumps({
+            "webPublicationDate": "2024-12-04T16:02:15Z",
+            "webTitle": "Georgia's payback machine: Fani Willis resists subpoenas by state lawmakers over Trump prosecution",
+            "webUrl": "https://www.theguardian.com/us-news/2024/dec/04/fani-willis-georgia-trump-trial"
+        })
+    },
+    {
+        "Id": "tv-tonight-an-exhilarating-night-of-music-with-florence-the-machine",
+        "MessageBody": dumps({
+            "webPublicationDate": "2024-10-19T05:00:08Z",
+            "webTitle": "TV tonight: an exhilarating night of music with Florence + the Machine ",
+            "webUrl": "https://www.theguardian.com/tv-and-radio/2024/oct/19/tv-tonight-an-exhilarating-night-of-music-with-florence-the-machine"
+        })
+    },
+    {
+        "Id": "joe-root-overhauls-alastair-cook-to-become-england-record-test-run-scorer",
+        "MessageBody": dumps({
+        "webPublicationDate": "2024-10-09T15:42:09Z",
+        "webTitle": "'Run-scoring machine': Alastair Cook praises Joe Root on passing landmark",
+        "webUrl": "https://www.theguardian.com/sport/2024/oct/09/joe-root-overhauls-alastair-cook-to-become-england-record-test-run-scorer"
+        })
+    }
+]
