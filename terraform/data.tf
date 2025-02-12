06191a9242891028f9eaf323e4cb41d19489b1ca
Martin Colbourne <martinlambc@gmail.com>
2025-02-12 13:49:14 +0100

    update data file

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,11 +1,11 @@
 data "archive_file" "lambda" {
     type        = "zip"
     source_dir  = "${path.module}/../lambda_app"
-    output_path = "${path.module}/../lambda_app.zip"
+    output_path = "${path.module}/../zip_files/lambda_app.zip"
 }
 
-# data "archive_file" "layer" {
-#   type = "zip"
-#   source_dir =  "${path.module}/../layer" 
-#   output_path =  "${path.module}/../layer.zip"
-# }
+data "archive_file" "layer" {
+  type = "zip"
+  source_dir =  "${path.module}/../layer" 
+  output_path =  "${path.module}/../zip_files/layer.zip"
+}
