a1877e2932190d206b5e356a7b42d7f623a92411
Martin Colbourne <martinlambc@gmail.com>
2025-02-15 17:36:41 +0100

    change pattern

--- terraform/cw.tf
+++ terraform/cw.tf
@@ -2,10 +2,10 @@ resource "aws_cloudwatch_metric_alarm" "critical_error_alarm" {
   alarm_name = "critical_error_alarm" 
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods = 1
-  period = 60
+  period = 120
   metric_name = aws_cloudwatch_log_metric_filter.lambda_crit_error_filter.name
   namespace = aws_cloudwatch_log_metric_filter.lambda_crit_error_filter.metric_transformation[0].namespace
-  statistic = "Minimum"
+  statistic = "Sum"
   threshold = 1
   alarm_actions = [aws_sns_topic.critical_error_notifications.arn]
 }
@@ -13,7 +13,7 @@ resource "aws_cloudwatch_metric_alarm" "critical_error_alarm" {
 resource "aws_cloudwatch_log_metric_filter" "lambda_crit_error_filter" {
   name = "lambda_critical_error_filter"
   log_group_name = "/aws/lambda/${var.lambda_name}"
-  pattern = "Critical error"
+  pattern = "{ $.message = %Critical error% }"
   metric_transformation {
     name = "critical_error"
     namespace = "critical_errors"
