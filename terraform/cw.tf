36de855b2fc75d0f7dcf69f76198db8a3bcd73f9
Martin Colbourne <martinlambc@gmail.com>
2025-02-15 16:50:21 +0100

    minor fix

--- terraform/cw.tf
+++ terraform/cw.tf
@@ -2,8 +2,9 @@ resource "aws_cloudwatch_metric_alarm" "critical_error_alarm" {
   alarm_name = "critical_error_alarm" 
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods = 1
+  period = 60
   metric_name = aws_cloudwatch_log_metric_filter.lambda_crit_error_filter.name
-  namespace = aws_cloudwatch_log_metric_filter.lambda_crit_error_filter.metric_transformation.namespace
+  namespace = aws_cloudwatch_log_metric_filter.lambda_crit_error_filter.metric_transformation[0].namespace
   statistic = "Minimum"
   threshold = 1
   alarm_actions = [aws_sns_topic.critical_error_notifications.arn]
