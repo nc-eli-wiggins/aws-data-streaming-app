4c4a71747d487844d6917b13515d5ad3ab804e2f
Martin Colbourne <martinlambc@gmail.com>
2025-02-20 17:21:38 +0100

    add request class and tests

---
+++ lambda_app/lambda_classes.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+
+class Request(BaseModel):
+    SearchTerm: str = Field(min_length=2)
+    FromDate: str = Field(default='', pattern=r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")
+    queue: Literal["guardian_content"]
---
+++ test/test_request.py
@@ -0,0 +1,60 @@
+from pydantic import ValidationError
+
+import pytest
+
+from lambda_app.lambda_classes import Request
+
+
+@pytest.mark.it("Validates when event contains valid SearchTerm and queue and no FromDate")
+def test_valid_request():
+    event = {'SearchTerm': "blob", "queue": "guardian_content"}
+    Request(**event)
+
+
+@pytest.mark.it("Validates when event contains valid SearchTerm, queue and FromDate")
+def test_valid_request_no_from():
+    event = {'SearchTerm': "blob", "FromDate": "2016-09-10", "queue": "guardian_content"}
+    Request(**event)
+
+
+@pytest.mark.it("Raises validation error when SearchTerm missing")
+def test_missing_search():
+    with pytest.raises(ValidationError):
+        event = {"FromDate": "2016-09-10", "queue": "guardian_content"}
+        Request(**event)
+
+
+@pytest.mark.it("Raises validation error when queue missing")
+def test_missing_queue():
+    with pytest.raises(ValidationError):
+        event = {'SearchTerm': "blob", "FromDate": "2016-09-10"}
+        Request(**event)
+
+
+@pytest.mark.it("Raises validation error when SearchTerm isn't string")
+def test_raises_err_search():
+    for x in [{}, [], (), 1, True]:
+        event = {'SearchTerm': x, "FromDate": "2016-09-10", "queue": "guardian_content"}
+        with pytest.raises(ValidationError):
+            Request(**event)
+
+
+@pytest.mark.it("Raises validation error when from date doesn't match yyyy-mm-dd format")
+def test_raises_err_date():
+    event = {'SearchTerm': "egg", "queue": "guardian_content", "FromDate": "20-02-1608"}
+    with pytest.raises(ValidationError):
+        Request(**event)
+
+
+@pytest.mark.it("Raises validation error if queue not 'guardian_content'")
+def test_queue_not_guardian():
+    event = {'SearchTerm': "egg", "queue": "guardian_content", "FromDate": "20-02-1608"}
+    with pytest.raises(ValidationError):
+        Request(**event)
+
+
+@pytest.mark.it("Raises validation error if SearchFrom < 2 chars")
+def test_queue_not_guardian():
+    event = {'SearchTerm': "e", "queue": "guardian_content", "FromDate": "2016-09-10"}
+    with pytest.raises(ValidationError):
+        Request(**event)
