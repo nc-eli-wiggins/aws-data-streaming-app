cab0bf377773465e221ecbbb40d90bbff5252af6
Martin Colbourne <martinlambc@gmail.com>
2025-02-28 17:52:49 +0100

    format and add doc string

--- lambda_app/lambda_classes.py
+++ lambda_app/lambda_classes.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel, Field
 from typing import Literal
 
+
 class LambdaEvent(BaseModel):
+    """Pydantic model for validating Lambda events"""
+
     SearchTerm: str = Field(min_length=2)
-    FromDate: str = Field(default='', pattern=r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$")
+    FromDate: str = Field(
+        default="", pattern=r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"
+    )
     queue: Literal["guardian_content"]
